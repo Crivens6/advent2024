292727e74cc514b2314ccf3039ddd7e95f3403ea
Crivens6 <ChrisHorch@sbcglobal.net>
2024-12-17 13:19:18 -0800

    Inital advent 12.2 attempt (incorrect)

--- 12/advent12_2.cpp
+++ 12/advent12_2.cpp
@@ -12,7 +12,7 @@ using std::string;
 using std::vector;
 
 // Constants
-const string kInputFilePath = "input.txt";
+const string kInputFilePath = "input_test.txt";
 const int kDirectionCoor[4][2] = {
     {-1, 0},
     {0, 1},
@@ -61,10 +61,11 @@ public:
 
         char plot_char = plot_grid[start_row][start_col];
         int area = 1;
-        int perimeter = 0;
 
         set<pair<int, int>> newly_accounted_plots;
         list<pair<int, int>> plots_to_analize;
+        // Start coord / end coord
+        set<pair<pair<int, int>, pair<int, int>>> plot_edges;
         newly_accounted_plots.insert({start_row, start_col});
         plots_to_analize.push_back({start_row, start_col});
         while (plots_to_analize.size() > 0)
@@ -79,7 +80,7 @@ public:
                 if (0 > adjacent_plot.first || adjacent_plot.first >= plot_grid.size() ||
                     0 > adjacent_plot.second || adjacent_plot.second >= plot_grid[0].size())
                 {
-                    perimeter++;
+                    plot_edges.insert(GetSideCoords(center_plot, dir));
                     continue;
                 }
                 // Check that plot not already found
@@ -95,14 +96,96 @@ public:
                     // If not correct char, add to perimeter
                     else
                     {
-                        perimeter++;
+                        plot_edges.insert(GetSideCoords(center_plot, dir));
                     }
                 }
             }
         }
-        int cost = area * perimeter;
-        // std::cout << plot_char << ": " << area << "*" << perimeter << "=" << cost << "\n";
+
+        int side_count = 0;
+        while (plot_edges.size() > 0)
+        {
+            pair<pair<int, int>, pair<int, int>> first_side_section = *plot_edges.begin();
+            pair<pair<int, int>, pair<int, int>> last_side_section = first_side_section;
+            plot_edges.erase(plot_edges.begin());
+            int dirCoor[2];
+            if (first_side_section.first.first == first_side_section.second.first)
+            {
+                dirCoor[0] = kDirectionCoor[Direction::W][0];
+                dirCoor[1] = kDirectionCoor[Direction::W][1];
+            }
+            else
+            {
+                dirCoor[0] = kDirectionCoor[Direction::N][0];
+                dirCoor[1] = kDirectionCoor[Direction::N][1];
+            }
+
+            // Check N / W for end of wall
+            bool side_searching_flag = true;
+            while (side_searching_flag)
+            {
+                pair<int, int> new_side_point = {first_side_section.first.first + dirCoor[0], first_side_section.first.second + dirCoor[1]};
+                pair<pair<int, int>, pair<int, int>> prior_side_section = {new_side_point, first_side_section.first};
+                // Check if reached end of side
+                if (plot_edges.find(prior_side_section) == plot_edges.end())
+                {
+                    side_searching_flag = false;
+                }
+                // Move to new endpoint and remove it from the set
+                else
+                {
+                    first_side_section = prior_side_section;
+                    plot_edges.erase(plot_edges.find(prior_side_section));
+                }
+            }
+            // Check S / E for end of wall
+            side_searching_flag = true;
+            while (side_searching_flag)
+            {
+                pair<int, int> new_side_point = {last_side_section.second.first - dirCoor[0], last_side_section.second.second - dirCoor[1]};
+                pair<pair<int, int>, pair<int, int>> next_side_section = {last_side_section.second, new_side_point};
+                // Check if reached end of side
+                if (plot_edges.find(next_side_section) == plot_edges.end())
+                {
+                    side_searching_flag = false;
+                }
+                // Move to new endpoint and remove it from the set
+                else
+                {
+                    last_side_section = next_side_section;
+                    plot_edges.erase(plot_edges.find(next_side_section));
+                }
+            }
+
+            // Count side removed from set
+            side_count++;
+        }
+
+        int cost = area * side_count;
+        std::cout << plot_char << ": " << area << "*" << side_count << "=" << cost << "\n";
         return {cost, newly_accounted_plots};
+    }
+    // Return in Top/Down or Left/Right format
+    pair<pair<int, int>, pair<int, int>> GetSideCoords(pair<int, int> plot_coords, int dir)
+    {
+        int row = plot_coords.first;
+        int col = plot_coords.second;
+        if (dir == Direction::N)
+        {
+            return {{row, col}, {row, col + 1}};
+        }
+        else if (dir == Direction::S)
+        {
+            return {{row + 1, col}, {row + 1, col + 1}};
+        }
+        else if (dir == Direction::E)
+        {
+            return {{row, col + 1}, {row + 1, col + 1}};
+        }
+        else
+        {
+            return {{row, col}, {row + 1, col}};
+        }
     }
 };
 
